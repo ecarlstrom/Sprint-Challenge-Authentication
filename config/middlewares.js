14a7aa571ad124efa131fc0b6f72dad6af8a9086
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-10-19 11:58:26 -0400

    added jwt secret and createToken() function to middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -5,6 +5,25 @@ const jwtKey = require('../_secrets/keys').jwtKey;
 // quickly see what this file exports
 module.exports = {
   authenticate,
+  createToken
+};
+
+// adding a jwt secret and token generation function for authentication
+
+const jwtSecret = 'SUPER SECRET KEY';
+
+function createToken(user) {
+
+  const jwtPayload = {
+    username: user.username
+  };
+
+  const jwtOptions = {
+    expiresIn: '30m',
+    jwtid: '123'
+  };
+
+  return jwt.sign(jwtPayload, jwtSecret, jwtOptions);
 };
 
 // implementation details
